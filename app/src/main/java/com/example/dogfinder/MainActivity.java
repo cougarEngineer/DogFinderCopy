c9c5d00fe61908ae0f920a869453aea0537446ae
Trevor <jam15005@byui.edu>
2020-02-18 15:48:06 -0700

    Trevor's Comment

--- app/src/main/java/com/example/dogfinder/MainActivity.java
+++ app/src/main/java/com/example/dogfinder/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //Michael's Comment
     //Brig Comment
+    //Trevor's Comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
