226f5db3ddb64f53ffda3ff944fb88958e9045b9
cougarEngineer <44527659+cougarEngineer@users.noreply.github.com>
2020-02-25 15:32:22 -0700

    Adding stubs to DogProfile, Breed, and Color for testing purposes

--- app/src/main/java/com/example/dogfinder/DogProfile.java
+++ app/src/main/java/com/example/dogfinder/DogProfile.java
@@ -1,4 +1,14 @@
 package com.example.dogfinder;
 
 public class DogProfile {
+    private Breed breed = Breed.Unknown;
+    private Color color = Color.Unknown;
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
