0bcceee04a27909bdbd3654bcd6e697762acfc8d
cougarEngineer <44527659+cougarEngineer@users.noreply.github.com>
2020-02-25 15:26:11 -0700

    Adding get stub for testing purposes

--- app/src/main/java/com/example/dogfinder/DogProfileDatabaseInteractor.java
+++ app/src/main/java/com/example/dogfinder/DogProfileDatabaseInteractor.java
@@ -1,4 +1,13 @@
 package com.example.dogfinder;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
+
 public class DogProfileDatabaseInteractor {
+
+    public static List<DogProfile> get() {
+        return new ArrayList<>();
+    }
 }
