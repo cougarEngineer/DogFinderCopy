0f799c21e31dbbc86d1748d35963f33adff51880
bjfranco <fra18020@byui.edu>
2020-02-25 15:43:10 -0700

    add tests

--- app/src/test/java/com/example/dogfinder/ExampleUnitTest.java
+++ app/src/test/java/com/example/dogfinder/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.example.dogfinder;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +15,24 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void gettingListTest() {
+        DogProfileDatabaseInteractor dog = new DogProfileDatabaseInteractor();
+        List<DogProfile> list = dog.get();
+        assertNotNull(list);
+    }
+
+    @Test
+    public void dogProfileColorTest() {
+        DogProfile profile = new DogProfile();
+        assertEquals("Default Color isn't Unknown", Color.Unknown, profile.getColor());
+    }
+
+    @Test
+    public void dogProfileBreedTest() {
+        DogProfile breed = new DogProfile();
+        assertEquals(Breed.Unknown, breed.getBreed());
     }
 }
