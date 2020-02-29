9fd0731b9fcb89607794a29d8249b75ac678e4ea
Trevor <jam15005@byui.edu>
2020-02-28 20:43:11 -0700

    Update MainActivity Button Functionality

--- app/src/main/java/com/example/dogfinder/MainActivity.java
+++ app/src/main/java/com/example/dogfinder/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.dogfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showList(View view) {
+        Intent i = new Intent(this, ListActivity.class);
+        startActivity(i);
+    }
+
+    public void showAdd(View view) {
+        Intent i = new Intent(this, AddActivity.class);
+        startActivity(i);
+    }
+
+    public void showSignIn(View view) {
+        Intent i = new Intent(this, SignInActivity.class);
+        startActivity(i);
     }
 }
 
