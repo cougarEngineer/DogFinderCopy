64b42a0f10502e2dea664ba40cb6a76eb03c0fdc
cougarEngineer <44527659+cougarEngineer@users.noreply.github.com>
2020-03-03 15:33:38 -0700

    Adding username to sharedPreferences

--- app/src/main/java/com/example/dogfinder/SignInActivity.java
+++ app/src/main/java/com/example/dogfinder/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.dogfinder;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class SignInActivity extends AppCompatActivity {
+
+    static final String USERNAME = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,24 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView tv = findViewById(R.id.currentUserTextView);
+        SharedPreferences mPrefs = getDefaultSharedPreferences(getApplicationContext());
+        String curUser = mPrefs.getString(USERNAME, "");
+        if (curUser.equals("")) {
+            tv.setText("Not signed in");
+        } else {
+            tv.setText("Signed in as " + curUser);
+        }
     }
 
+    public void onClickSignInButton(View view) {
+        EditText editText = findViewById(R.id.usernameField);
+        String username = editText.getText().toString();
+        TextView tv = findViewById(R.id.currentUserTextView);
+        tv.setText("Signed in as " + username);
+        SharedPreferences mPrefs = getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        prefsEditor.putString(USERNAME, username);
+        prefsEditor.apply();
+    }
 }
