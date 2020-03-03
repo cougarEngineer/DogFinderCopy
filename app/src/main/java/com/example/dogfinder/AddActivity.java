e01ea9a793bc2cbfe809c5ea5e4be1af0df92c43
eg661 <eg6616@gmail.com>
2020-03-03 15:43:01 -0700

    changes to add and showprofile

--- app/src/main/java/com/example/dogfinder/AddActivity.java
+++ app/src/main/java/com/example/dogfinder/AddActivity.java
@@ -1,5 +1,6 @@
 package com.example.dogfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +10,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddActivity extends AppCompatActivity {
+
+    EditText name, picture, height, weight, info, city, state, address, contact;
+    Spinner breed, color;
+    Button create, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        //Hooks
+        name = findViewById(R.id.nameET);
+        picture = findViewById(R.id.urlET);
+        breed = findViewById(R.id.breedSpinner);
+        color = findViewById(R.id.colorSpinner);
+        height = findViewById(R.id.heightET);
+        weight = findViewById(R.id.weightET);
+        info = findViewById(R.id.infoET);
+        city = findViewById(R.id.cityET);
+        state = findViewById(R.id.stateET);
+        address = findViewById(R.id.addressET);
+        contact = findViewById(R.id.contactET);
+        create = findViewById(R.id.createBtn);
+        breed.setAdapter(new ArrayAdapter<Breed>(this, android.R.layout.simple_spinner_item, Breed.values()));
+        color.setAdapter(new ArrayAdapter<Color>(this, android.R.layout.simple_spinner_item, Color.values()));
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //****************Get User Input**********
+                String nameS = name.getText().toString();
+                String heightS = height.getText().toString();
+                String weightS = weight.getText().toString();
+                String infoS = info.getText().toString();
+                String cityS = city.getText().toString();
+                String addressS = address.getText().toString();
+                String contactS = contact.getText().toString();
+                String breedS = breed.getSelectedItem().toString();
+                String colorS = color.getSelectedItem().toString();
+
+                Intent intent = new Intent(AddActivity.this, ShowProfileActivity.class);
+                intent.putExtra("name", nameS);
+                intent.putExtra("height",heightS);
+                intent.putExtra("weight",weightS);
+                intent.putExtra("info",infoS);
+                intent.putExtra("city",cityS);
+                intent.putExtra("address",addressS);
+                intent.putExtra("contact",contactS);
+                intent.putExtra("breed",breedS);
+                intent.putExtra("color",colorS);
+                startActivity(intent);
+                //****************************************
+            }
+        });
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/example/dogfinder/ShowProfileActivity.java
+++ app/src/main/java/com/example/dogfinder/ShowProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.dogfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +10,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 public class ShowProfileActivity extends AppCompatActivity {
+
+    TextView name, breed, color, height, weight, info, city, state, address, contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_profile);
+
+        name = findViewById(R.id.getName);
+        breed = findViewById(R.id.getBreed);
+        color = findViewById(R.id.getColor);
+        height = findViewById(R.id.getHeight);
+        weight = findViewById(R.id.getWeight);
+        info = findViewById(R.id.getInfo);
+        city = findViewById(R.id.getCity);
+        state = findViewById(R.id.getState);
+        address = findViewById(R.id.getAddress);
+        contact = findViewById(R.id.getContact);
+
+        //*******Display Profile Check******
+        Intent intent = getIntent();
+        String nameS = intent.getStringExtra("name");
+        String heightS = intent.getStringExtra("height");
+        String weightS = intent.getStringExtra("weight");
+        String infoS = intent.getStringExtra("info");
+        String cityS = intent.getStringExtra("city");
+        String addressS = intent.getStringExtra("address");
+        String contactS = intent.getStringExtra("contact");
+        String breedS = intent.getStringExtra("breed");
+        String colorS = intent.getStringExtra("color");
+        name.setText(nameS);
+        height.setText(heightS);
+        weight.setText(weightS);
+        info.setText(infoS);
+        city.setText(cityS);
+        address.setText(addressS);
+        contact.setText(contactS);
+        breed.setText(breedS);
+        color.setText(colorS);
+        //*********************************
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
