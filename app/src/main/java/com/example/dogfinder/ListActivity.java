1d0047436a9c3f1f0e208e339b5c483420541cee
bjfranco <fra18020@byui.edu>
2020-03-03 13:10:37 -0700

    Updating SignIn and List GUI

--- app/src/main/java/com/example/dogfinder/ListActivity.java
+++ app/src/main/java/com/example/dogfinder/ListActivity.java
@@ -9,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -18,16 +22,32 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /*
+        //These notes below can be deleted - notes to self for testing.
+
+        //test example using a string array - will change for actual Dog Profile ArrayList
+        String[] testText = {"Dog1", "Dog2", "Dog3", "Dog4", "Dog5", "Dog6", "Dog7"};
+        ListAdapter profileAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, testText);
+        ListView profileListView = (ListView) findViewById(R.id.profileList);
+        profileListView.setAdapter(profileAdapter);
+
+        //click item on list
+        profileListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        //what happens when clicked
+                        //example with String
+                        String dog = String.valueOf(parent.getItemAtPosition(position));
+                    }
+                }
+        );
+        */
+
     }
 
 }
