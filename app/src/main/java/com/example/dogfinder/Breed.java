ba3dd757a4cbd839bb6b07141f4603c4ac82153e
Trevor <jam15005@byui.edu>
2020-03-28 20:59:30 -0600

    Alphabetize Breeds

--- app/src/main/java/com/example/dogfinder/Breed.java
+++ app/src/main/java/com/example/dogfinder/Breed.java
@@ -5,9 +5,9 @@ package com.example.dogfinder;
  */
 public enum Breed {
     Unknown,
-    LabradorRetriever, GermanShepherd, GoldenRetriever, FrenchBulldog, Bulldog, Beagle,
-    Poodle, Rottweiler, Terrier, GermanShorthairedPointer, Boxer, Husky, Dachshund,
-    GreatDane, Corgi, Doberman, AustralianShepherd, Schnauzer, Spaniel, ShihTzu, Pomeranian,
-    Havanese, Sheepdog, Brittany, Mastiff, Pug, Chihuahua, Maltese, Newfoundland, BorderCollie,
-    BassetHound, Collie, ShibaInu, SaintBernard, Bloodhound
+    AustralianShepherd, BassetHound, Beagle, Bloodhound, BorderCollie, Boxer, Brittany, Bulldog,
+    Chihuahua, Collie, Corgi, Dachshund, Doberman, FrenchBulldog, GermanShepherd,
+    GermanShorthairedPointer, GoldenRetriever, GreatDane, Havanese, Husky, LabradorRetriever,
+    Maltese, Mastiff, Newfoundland, Pomeranian, Poodle, Pug, Rottweiler, SaintBernard, Samoan,
+    Schnauzer, Sheepdog, ShibaInu, ShihTzu, Spaniel, Terrier
 }
